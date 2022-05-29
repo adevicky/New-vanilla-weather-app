778504a20069646466dbd757c60d9a3aaae43bc5
Adenuga Victoria <adenugavictoria29@gmail.com>
2022-05-29 23:33:47 +0000

    Added images and id to body

--- src/app.js
+++ src/app.js
@@ -12,10 +12,11 @@ if (minutes < 10) {
 
 return `${day}, ${hours}:${minutes}`;
 }
+
 let now= new Date();
 let realTme= document.querySelector("#current-time");
 realTme.innerHTML=timeNow(now)
-
+        
 function currentDay(timestamp) {
   let date= new Date(timestamp * 1000);
   let day= date.getDay();
@@ -69,30 +70,31 @@ let description=document.querySelector("#description");
 let humidity= document.querySelector("#humidity");
 let wind= document.querySelector("#wind");
 let iconElement= document.querySelector("#icon");
-let bodyElement= document.querySelector("bTemp");
-let bTemp = {
-'bTemp_1' : 'https://s3.amazonaws.com/shecodesio-production/uploads/files/000/035/691/original/10.jpg?1653852586',
-'bTemp_2' : 'https://s3.amazonaws.com/shecodesio-production/uploads/files/000/035/690/original/sunny.jpg?1653852236',
-'bTemp_3' : 'https://s3.amazonaws.com/shecodesio-production/uploads/files/000/035/716/original/danny-jongerius-wJjAbEt_ZAw-unsplash.jpg?1653861259',
-'bTemp_4' : 'https://s3.amazonaws.com/shecodesio-production/uploads/files/000/035/693/original/cloudy-sky.jpg?1653853034',
-'bTemp_5' : 'https://s3.amazonaws.com/shecodesio-production/uploads/files/000/035/694/original/cloud.jpg?1653855724'
+let bodyElement= document.querySelector("#bg");
+let bg = {
+'bg_1' : 'https://s3.amazonaws.com/shecodesio-production/uploads/files/000/035/691/original/10.jpg?1653852586',
+'bg_2' : 'https://s3.amazonaws.com/shecodesio-production/uploads/files/000/035/692/original/partly-cloudy.jpg?1653852923',
+'bg_3' : 'https://s3.amazonaws.com/shecodesio-production/uploads/files/000/035/690/original/sunny.jpg?1653852236',
+'bg_4' : 'https://s3.amazonaws.com/shecodesio-production/uploads/files/000/035/693/original/cloudy-sky.jpg?1653853034',
 };
-let tempBg = response.data.main.temp;
-let bTempImg = "";
 
-if(tempBg > 100){
-    bTempImg = "bTemp_1";
-}else if(tempBg <= 25) {
-  bTempImg="bTemp_4";
-}else if(tempBg<= 30){
-  bTempImg= "bTemp_5";
-}else if(tempBg<= 40){
-  bTempImg= "bTemp_2";
-}else {
-  bTempImg= "bTemp_3"
+let tempEr = response.data.main.temp;
+let bgImg = "";
+
+if(tempEr <= 25){
+    bgImg = "bg_1";
+}
+else if(tempEr <= 30){
+    bgImg = "bg_2";
+}
+else if(tempEr <= 40){
+    bgImg = "bg_3";
+}
+else{
+    bgImg = "bg_4";
 }
 
-
+  celsiusTemperature = response.data.main.temp;
 
 newLocation.innerHTML=response.data.name;
 temprature .innerHTML= Math.round(response.data.main.temp);
@@ -101,7 +103,7 @@ humidity.innerHTML = response.data.main.humidity;
 wind.innerHTML = response.data.wind.speed;
 iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt",response.data.weather[0].main);
-bodyElement.style.backgroundImage = "url('"+ bTemp[bTempImg] +"')";
+bodyElement.style.backgroundImage = "url('"+ bg[bgImg] +"')";
 getForecast(response.data.coord);
 }
 
@@ -110,6 +112,7 @@ function search(city) {
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayCurrentWeather); 
 }
+
 
 function handleSubmit(event)  {
     event.preventDefault();
@@ -133,11 +136,50 @@ function locationButton(event) {
 
 let currentLocation= document.querySelector("#current-icon");
 currentLocation.addEventListener("click", locationButton);
-
-let city="Paris";
+ let city= "Okitipupa";
 let apiKey= "654e7c2b1cb01736424b3824e69350d2";
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayCurrentWeather); 
+
+
+function convertToFarhenheit(event){
+  event.preventDefault();
+  let tempratureElement= document.querySelector("#temprature"); 
+  celsiusLink.classList.remove("active");
+  farhenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+    tempratureElement.innerHTML = Math.round(fahrenheiTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+farhenheitLink.classList.remove("active");
+  let tempratureElement = document.querySelector("#temprature");
+  tempratureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let farhenheitLink= document.querySelector("#farhenheit-link");
+farhenheitLink.addEventListener("click",convertToFarhenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
